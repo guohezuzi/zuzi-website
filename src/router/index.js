c2dac7d4efe0fe9b440349305bd110be9ad3e0c2
guohezuzi <987467637@qq.com>
2019-04-03 13:53:17 +0800

    增加私密文章模块,同时对router进行重构,以及share.css中将默认p的样式重构

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import axios from 'axios'
 import store from '../store/index'
 import Error from '../views/Error'
+import BaseTpl from '../components/BaseTpl'
 import IndexHeadContent from '../views/index/IndexHeadContent'
 import IndexMainContent from '../views/index/IndexMainContent'
 import AboutMeHeadContent from '../views/aboutme/AboutMeHeadContent'
@@ -11,6 +12,7 @@ import ContactHeadContent from '../views/contact/ContactHeadContent'
 import ContactMainContent from '../views/contact/ContactMainContent'
 import ArticleHeadContent from '../views/article/ArticleHeadContent'
 import ArticleMainContent from '../views/article/ArticleMainContent'
+import SecretArticle from '../views/secret/SecretArticle'
 
 
 Vue.use(Router)
@@ -27,72 +29,90 @@ const router = new Router({
   routes: [
     {
       path: '/',
-      name: 'index',
-      components: {
-        headContent: IndexHeadContent,
-        mainContent: IndexMainContent,
-      },
-      meta: {
-        title: 'ZUZI-BLOG-首页'
-      }
-    },
-    {
-      path: '/aboutme',
-      name: 'aboutMe',
-      components: {
-        headContent: AboutMeHeadContent,
-        mainContent: AboutMeMainContent,
-      },
-      meta: {
-        title: 'ZUZI-BLOG-关于我'
-      }
-    },
-    {
-      path: '/contact',
-      name: 'contact',
-      components: {
-        headContent: ContactHeadContent,
-        mainContent: ContactMainContent
-      },
-      meta: {
-        title: 'ZUZI-BLOG-联系我'
-      }
-    },
-    {
-      path: '/article/:articleVisitUrl',
-      name: 'article',
-      components: {
-        headContent: ArticleHeadContent,
-        mainContent: ArticleMainContent
-      },
-      meta: {
-        title: 'ZUZI-BLOG-文章详情'
-      },
-      beforeEnter(to, from, next) {
-        if (Object.keys(store.state.article).length !== 0) {
-          next()
-        } else {
-          const url = "/api/articles/" + to.params.articleVisitUrl
-          axios.get(url).then((res) => {
-            store.commit('update', res.data)
-            next()
-          }).catch(() => {
-            next('/error')
-          })
+      name: 'base_tpl',
+      component: BaseTpl,
+      redirect: {name: 'index'},
+      children:[{
+        path: "/",
+        name: "index",
+        components: {
+          headContent: IndexHeadContent,
+          mainContent: IndexMainContent,
+        },
+        meta: {
+          title: 'ZUZI-BLOG-首页'
         }
-      }
-    },
-    {
-      path: '/error',
-      name: 'error',
-      components: {
-        headContent: Error,
-        mainContent: null
       },
-      meta: {
-        title: 'ZUZI-BLOG-ERROR'
-      }
-    }
+        {
+          path: 'aboutme',
+          name: 'aboutMe',
+          components: {
+            headContent: AboutMeHeadContent,
+            mainContent: AboutMeMainContent,
+          },
+          meta: {
+            title: 'ZUZI-BLOG-关于我'
+          }
+        },
+        {
+          path: 'contact',
+          name: 'contact',
+          components: {
+            headContent: ContactHeadContent,
+            mainContent: ContactMainContent
+          },
+          meta: {
+            title: 'ZUZI-BLOG-联系我'
+          }
+        },
+        {
+          path: 'article/:articleVisitUrl',
+          name: 'article',
+          components: {
+            headContent: ArticleHeadContent,
+            mainContent: ArticleMainContent
+          },
+          meta: {
+            title: 'ZUZI-BLOG-文章详情'
+          },
+          beforeEnter(to, from, next) {
+            if (Object.keys(store.state.article).length !== 0) {
+              next()
+            } else {
+              const url = "/api/articles/" + to.params.articleVisitUrl
+              axios.get(url).then((res) => {
+                store.commit('update', res.data)
+                next()
+              }).catch(() => {
+                next('/error')
+              })
+            }
+          }
+        },
+        {
+          path: 'error',
+          name: 'error',
+          components: {
+            headContent: Error,
+            mainContent: null
+          },
+          meta: {
+            title: 'ZUZI-BLOG-ERROR'
+          }
+        },
+        {
+          path: '/article/secret/:articleVisitUrl',
+          name: 'secret_article',
+          components: {
+            headContent: SecretArticle,
+            mainContent: null
+          },
+          meta: {
+            title: 'ZUZI-BLOG-私密文章'
+          },
+        },
+      ]
+    },
   ]
 })
 
