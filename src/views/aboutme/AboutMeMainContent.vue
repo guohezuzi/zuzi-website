9350c3442083569b5ff6cebee78d377b926ee9b7
guohezuzi <987467637@qq.com>
2019-04-03 13:54:32 +0800

    change about me

--- src/views/aboutme/AboutMeMainContent.vue
+++ src/views/aboutme/AboutMeMainContent.vue
@@ -5,7 +5,7 @@
       同时在这个过程中，我能很快的集中注意力，或许达到了心流状态吧，并且我知道每一步去做什么，这种感觉真的非常棒！
       另一方面，我也是一个终生学习者，因为通过系统的学习，然后加入自己的思维方式或者想法，再将所学的东西去输出，
       这个过程真的很棒！并且学习过程的确定性，可叠加性，还有学习的复利效应，简直就是解开这个世界的万能钥匙。另外，
-      说点具体的吧，当写下这篇介绍的时候，我是一名大三的学生，一名全栈增长性工程师，目前的精通的技术栈是JavaWeb、
+      说点具体的吧，当写下这篇介绍的时候，我是一名大三的学生，一名全栈增长性工程师，目前的掌握的技术栈是JavaWeb、
       Vue.js, 正在学习Java大数据技术，空闲时间在了解flutter框架。如果你对我感兴趣，可以通过下面的图标链接了解到更多。</p>
     <ShareIcons/>
   </div>
@@ -14,7 +14,7 @@
 <script>
   import ShareIcons from "../../components/ShareIcons";
 
-    export default {
+  export default {
         name: "AboutMeMainContent",
         components: {ShareIcons}
     }
@@ -33,6 +33,7 @@
     margin: 5vh 5vw;
     font-size: 2.5vh;
     overflow: hidden;
+    letter-spacing: 0.1rem;
   }
 
   #about-me-detail >>> .icon {
