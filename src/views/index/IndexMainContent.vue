30691cc2a30f09dceae916cfc086aecdec8481eb
zuzi <guohezuzip@gmail.com>
2019-07-21 01:45:21 +0800

    首页文章分页显示

--- src/views/index/IndexMainContent.vue
+++ src/views/index/IndexMainContent.vue
@@ -1,92 +1,125 @@
 <template>
   <div>
     <div id="article-items">
-      <ArticleItem
+      <template v-for="articles in articleList">
+        <ArticleItem
           v-for="(article,index) in articles"
           :index="index"
           :article="article"
           :key="article.articleId"
-      />
+        />
+      </template>
+    </div>
+    <div class="loading" v-if="loading">
+      <atom-spinner :animation-duration="1000" :size="60" :color="'#ECF0F1'" />
     </div>
     <div id="read-more">
       <p>过往不恋 未来不迎 当时不杂</p>
       <div id="read-more-button-wrap">
-        <a id="read-more-button" href="#">阅读更多文章</a>
+        <a id="read-more-button" href="#" @click.prevent="loadmore()">阅读更多文章</a>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-  import ArticleItem from "../../components/ArticleItem";
-  import debounce  from 'lodash/debounce'
+import ArticleItem from "../../components/ArticleItem";
+import debounce from "lodash/debounce";
+import { AtomSpinner } from "epic-spinners";
 
-  export default {
-    name: "IndexMainContent",
-    components: {ArticleItem},
-    data() {
-      return {
-        articles: null
-      }
-    },
-    mounted() {
-      this.$http.get('/api/articles?pageNum=1&pageSize=4')
-        .then(response => (this.articles = response.data))
-        .catch(error => console.log(error));
-      document.addEventListener("scroll", this.handleScroll, false);
-    },
-    methods: {
-      handleScroll: debounce(function () {
-        const articleItems = document.getElementById('article-items').childNodes;
-        for (let i of articleItems) {
-          let diff = window.innerHeight - i.getBoundingClientRect().top;
-          if (diff > 0 && i.getBoundingClientRect().bottom > 0) {
-            let img = i.querySelector('img');
-            img.style.animation = 'fadeIn 1s';
-            img.src = img.getAttribute('data-url');
-          }
+export default {
+  name: "IndexMainContent",
+  components: {
+    ArticleItem,
+    AtomSpinner
+  },
+  data() {
+    return {
+      loading: true,
+      currPage: 0,
+      lastPage: 1,
+      articleList: []
+    };
+  },
+  mounted() {
+    this.loadmore();
+    document.addEventListener("scroll", this.handleScroll, false);
+  },
+  methods: {
+    handleScroll: debounce(function() {
+      const articleItems = document.getElementById("article-items").childNodes;
+      console.log(articleItems.length);
+      for (let i of articleItems) {
+        let diff = window.innerHeight - i.getBoundingClientRect().top;
+        if (diff > 0 && i.getBoundingClientRect().bottom > 0) {
+          let img = i.querySelector("img");
+          img.style.animation = "fadeIn 1s";
+          img.src = img.getAttribute("data-url");
         }
-      },100),
-    },
-    destroyed() {
-      document.removeEventListener('scroll', this.handleScroll);
+      }
+    }, 100),
+    loadmore() {
+      if (this.currPage < this.lastPage) {
+        this.currPage++;
+        this.loading = true;
+        this.$http
+          .get("/api/articles?page=true&pageSize=4&pageNum=" + this.currPage)
+          .then(
+            response => (
+              (this.loading = false),
+              this.articleList.push(response.data.data),
+              (this.lastPage = response.data.last_page)
+            )
+          )
+          .catch(error => console.log(error));
+      }
     }
+  },
+  destroyed() {
+    document.removeEventListener("scroll", this.handleScroll);
   }
+};
 </script>
 
 <style scoped>
-  #read-more {
-    background-image: url('/public/img/Layer12.jpg');
-    background-size: cover;
-    height: 40vh;
-    width: 100%;
-    overflow: hidden;
-  }
+#read-more {
+  background-image: url("/public/img/Layer12.jpg");
+  background-size: cover;
+  height: 40vh;
+  width: 100%;
+  overflow: hidden;
+}
 
-  #read-more p {
-    letter-spacing: 0.8vw;
-    text-align: center;
-    font-size: 2.5vh;
-    margin: 10vh auto;
-  }
+#read-more p {
+  letter-spacing: 0.8vw;
+  text-align: center;
+  font-size: 2.5vh;
+  margin: 10vh auto;
+}
 
-  #read-more-button-wrap {
-    display: flex;
-    justify-content: center;
-  }
+#read-more-button-wrap {
+  display: flex;
+  justify-content: center;
+}
 
-  #read-more-button {
-    letter-spacing: 1vmin;
-    padding: 0 10vmin;
-    font-weight: bold;
-    line-height: 5vh;
-    border: 1px solid white;
-    text-align: center;
-  }
+#read-more-button {
+  letter-spacing: 1vmin;
+  padding: 0 10vmin;
+  font-weight: bold;
+  line-height: 5vh;
+  border: 1px solid white;
+  text-align: center;
+}
 
-  #read-more-button:hover {
-    border: 1px solid black;
-    color: black;
-    animation: tada 1s;
-  }
+#read-more-button:hover {
+  border: 1px solid black;
+  color: black;
+  animation: tada 1s;
+}
+
+.loading {
+  display: flex;
+  justify-content: center;
+  background-color: #1c2023;
+}
 </style>
